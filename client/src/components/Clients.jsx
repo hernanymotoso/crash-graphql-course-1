d57aecf1a0844cd765063c175f7448afa62946f5
Hernany Motoso <contatohernany@gmail.com>
2022-11-19 20:02:43 -0300

    refactor(client): move `GET_CLIENTS` to a query file

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,17 +1,6 @@
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { GET_CLIENTS } from '../queries/client.queries';
 import ClientRow from './ClientRow';
-
-
-const GET_CLIENTS = gql`
-    query getClients {
-        clients {
-            id
-            name
-            email
-            phone
-        }
-    }
-`
 
 export default function Clients() {
   const { loading, error, data} = useQuery(GET_CLIENTS);
