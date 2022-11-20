922a7b13e7c65c852914a86e17a16a384ce88112
Hernany Motoso <contatohernany@gmail.com>
2022-11-20 11:08:08 -0300

    feat(client): create `Home` & `NotFound` pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
-import Clients from "./components/Clients";
-import AddClientModal from "./components/AddClientModal";
-import Projects from "./components/Projects";
 
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -34,15 +34,17 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header/> 
-        
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div> 
+        <Router>
+          <Header/> 
+          
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div> 
+        </Router>
       </ApolloProvider>
-    
     </>
 
   );
