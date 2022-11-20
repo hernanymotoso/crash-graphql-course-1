8c01b656255f0a1322af8bf2291357379f39c935
Hernany Motoso <contatohernany@gmail.com>
2022-11-20 11:51:30 -0300

    feat(client): create `Project` page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
+import Project from './pages/Project';
 import NotFound from './pages/NotFound';
 
 const cache = new InMemoryCache({
@@ -40,6 +41,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
               <Route path='*' element={<NotFound />} />
             </Routes>
           </div> 
