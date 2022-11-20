44004a4ce3bfbf8f6b913a6e40fe7bd982146c42
Hernany Motoso <contatohernany@gmail.com>
2022-11-20 10:46:15 -0300

    feat(client): create `Project` & `Projects` components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Clients from "./components/Clients";
 import AddClientModal from "./components/AddClientModal";
+import Projects from "./components/Projects";
 
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
@@ -37,7 +38,8 @@ function App() {
         
         <div className="container">
           <AddClientModal />
-         <Clients />
+          <Projects />
+          <Clients />
         </div> 
       </ApolloProvider>
     
